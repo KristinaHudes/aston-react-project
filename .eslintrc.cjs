9bbb038c9526e1cd5b4f409fa2271ba9e23e352d
KristinaHudes <kris4742@gmail.com>
2024-03-19 19:27:32 +0300

    init: start aston react project

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'no-console': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-unused-expressions': 'error',
+    'react/jsx-no-target-blank': 'off',
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+  },
+};
