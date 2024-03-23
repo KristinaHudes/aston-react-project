32ae0c422e5dc5708c3e801eaeeae2194e549132
KristinaHudes <kris4742@gmail.com>
2024-03-23 03:36:09 +0300

    feat: implement switching to the page of a specific movie

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,21 +1,25 @@
+import { Link } from 'react-router-dom';
+
 import './MovieCard.scss';
 
 export const MovieCard = ({ data }) => {
-  const { Poster: poster, Title: title, Year: year } = data;
+  const { Poster: poster, Title: title, Year: year, imdbID } = data;
 
   return (
     <div className="card-item">
-      <div className="card-inner">
-        <div className="card-top">
-          <img src={poster} alt={title} />
-        </div>
-        <div className="card-bottom">
-          <div className="card-info">
-            <h4>{title}</h4>
-            <p>{year}</p>
+      <Link to={`/movie/${imdbID}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img src={poster} alt={title} />
+          </div>
+          <div className="card-bottom">
+            <div className="card-info">
+              <h4>{title}</h4>
+              <p>{year}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/components/SingleMovie/SingleMovie.jsx
+++ src/components/SingleMovie/SingleMovie.jsx
@@ -1,5 +1,75 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import {
+  fetchAsyncSingleMovie,
+  getSelectedMovie,
+  removeSelectedMovie,
+} from '../../features/movies/movieSlice';
 import './SingleMovie.scss';
 
 export const SingleMovie = () => {
-  return <div>Single Movie</div>;
+  const { imdbID } = useParams();
+  const dispatch = useDispatch();
+  const data = useSelector(getSelectedMovie);
+
+  const {
+    Title: title,
+    Plot: plot,
+    Director: director,
+    Actors: actors,
+    Genre: genre,
+    Language: language,
+    Awards: awards,
+    Poster: poster,
+  } = data;
+
+  useEffect(() => {
+    dispatch(fetchAsyncSingleMovie(imdbID));
+
+    return () => {
+      dispatch(removeSelectedMovie());
+    };
+  }, [dispatch, imdbID]);
+
+  return (
+    <div className="movie-section">
+      {Object.keys(data).length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{title}</div>
+            <div className="movie-plot">{plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{actors}</span>
+              </div>
+              <div>
+                <span>Genres</span>
+                <span>{genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{awards}</span>
+              </div>
+            </div>
+          </div>
+          <div className="section-right">
+            <img src={poster} alt={title} />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
